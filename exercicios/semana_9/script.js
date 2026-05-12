153f5e59efd7143d70f4a25be6f4a301183229a0
zZD4rkN1gh7Xx <luciano2607ferreira@gmail.com>
2026-05-12 11:24:40 +0100

    ex 4 semana 9 done

--- exercicios/semana_9/script.js
+++ exercicios/semana_9/script.js
@@ -1,10 +1,24 @@
 function attachBuyEvents() {
     const buttons = document.querySelectorAll('#products button');
 
-    for(const button of buttons){
-        button.addEventListener('click', function(e){
-            console.log('BUY!!');
-            console.log(e.currentTarget);
+    for (const button of buttons) {
+        button.addEventListener('click', function (e) {
+
+            const article = e.currentTarget.parentElement;
+            console.log(article);
+
+            article.classList.toggle('sale');
+
+            const id = article.getAttribute('data-id');
+            console.log('Product ID:', id);
+
+            const name = article.querySelector('h2').textContent;
+            const price = article.querySelector('.price').textContent;
+            const quantity = article.querySelector('input').value;
+
+            console.log('Name:', name);
+            console.log('Price:', price);
+            console.log('Quantity:', quantity);
         })
     }
 }
